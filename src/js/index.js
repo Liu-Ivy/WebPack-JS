194904001bd2e8a4d63469f160703b0114e1c4ec
Ivy Liu <ivyliu402@gmail.com>
2020-05-14 20:28:32 +0200

    input button done

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,31 @@
-// Global app controller
-import num from "./test";
+import Search from "./models/Search";
 
-console.log(`I imported ${num} from module on test.js file`);
+/** Global state for the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping lish object
+ * - Like recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+  //1)get query from view
+  const query = "pizza";
+
+  if (query) {
+    //2) New search object and add to state
+    state.search = new Search(query);
+    //3) prepare UI for results
+
+    //4) Search for recipes;
+    await state.search.getResults();
+
+    //5) Render results in UI
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector(".search").addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -16,4 +16,45 @@ module.exports = {
       template: "./src/index.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
 };
+
+/*
+module.exports = {
+  entry: ['babel-polyfill', './src/js/index.js'],
+  output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'js/bundle.js'
+  },
+  devServer: {
+      contentBase: './dist'
+  },
+  plugins: [
+      new HtmlWebpackPlugin({
+          filename: 'index.html',
+          template: './src/index.html'
+      })
+  ],
+  module: {
+      rules: [
+          {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              use: {
+                  loader: 'babel-loader'
+              }
+          }
+      ]
+  }
+};
+*/
