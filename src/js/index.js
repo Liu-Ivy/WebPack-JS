bccd5b812d74236fdbb59c26cb5641b13357ab0d
Ivy Liu <ivyliu402@gmail.com>
2020-05-18 19:59:01 +0200

    getting the loading

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,42 @@
 import Search from "./models/Search";
+import * as searchView from "./views/searchView";
+import { elements, renderLoader, clearLoader } from "./views/base";
 
 /** Global state for the app
  * - Search object
  * - Current recipe object
- * - Shopping lish object
+ * - Shopping list object
  * - Like recipes
  */
 const state = {};
 
 const controlSearch = async () => {
   //1)get query from view
-  const query = "pizza";
+  const query = searchView.getInput();
 
   if (query) {
-    //2) New search object and add to state
+    // 2) New search object and add to state
     state.search = new Search(query);
-    //3) prepare UI for results
 
-    //4) Search for recipes;
-    await state.search.getResults();
+    // 3) Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
+    renderLoader(elements.searchRes);
 
-    //5) Render results in UI
-    console.log(state.search.result);
+    try {
+      // 4) Search for recipes
+      await state.search.getResults();
+
+      // 5) Render results on UI
+      clearLoader();
+      searchView.renderResults(state.search.result);
+    } catch (err) {
+      alert("Something wrong with the search...");
+      clearLoader();
+    }
   }
 };
-
-document.querySelector(".search").addEventListener("submit", (e) => {
+elements.searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   controlSearch();
 });
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,1 +1,53 @@
+import { elements } from "./base";
 
+export const getInput = () => elements.searchInput.value;
+
+export const clearInput = () => {
+  elements.searchInput.value = " ";
+};
+
+export const clearResults = () => {
+  elements.searchResList.innerHTML = " ";
+};
+
+/*
+'Pasta with tomato and spinach'
+acc: 0 cur:Pasta / acc + cur.length = 5 / newTitle = ['Pasta'];
+acc: 5 cur:with / acc + cur.length = 9 / newTitle = ['Pasta with'];
+acc: 9 cur:tomato / acc + cur.length = 15 / newTitle = ['Pasta with tomato'];
+*/
+
+const limitRecipeTitle = (title, limit = 17) => {
+  const newTitle = [];
+  if (title.length > limit) {
+    title.split(" ").reduce((acc, cur) => {
+      if (acc + cur.length <= limit) {
+        newTitle.push(cur);
+      }
+      return acc + cur.length;
+    }, 0);
+    return `${newTitle.join(" ")}...`;
+  }
+  return title;
+};
+
+const renderRecipe = (recipe) => {
+  const markup = `
+  <li>
+      <a class="likes__link" href=${recipe.recipe_id}>
+          <figure class="likes__fig">
+              <img src=${recipe.image_url} alt=${recipe.title}>
+          </figure>
+          <div class="likes__data">
+              <h4 class="likes__name">${limitRecipeTitle(recipe.title)}</h4>
+              <p class="likes__author">${recipe.publisher}</p>
+          </div>
+      </a>
+  </li>
+  `;
+  elements.searchResList.insertAdjacentHTML("beforeend", markup);
+};
+
+export const renderResults = (recipe) => {
+  recipe.forEach(renderRecipe);
+};
